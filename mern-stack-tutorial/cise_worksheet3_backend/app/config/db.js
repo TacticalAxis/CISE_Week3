12009357f46c50d3a9c48ca3e0f0dc5398629d64
Nathan <nathan.dsouza316@gmail.com>
2023-08-05 13:23:04 +1200

    Update MERN Stack to use db URI

--- mern-stack-tutorial/cise_worksheet3_backend/app/config/db.js
+++ mern-stack-tutorial/cise_worksheet3_backend/app/config/db.js
@@ -1,11 +1,6 @@
 const mongoose = require('mongoose')
 
-const MONGO_USER = process.env.MONGO_USER
-const MONGO_PASS = process.env.MONGO_PASS
-const MONGO_HOST = process.env.MONGO_HOST
-const MONGO_DB = process.env.MONGO_DB
-
-const db = `mongodb+srv://${MONGO_USER}:${MONGO_PASS}@${MONGO_HOST}/${MONGO_DB}?retryWrites=true&w=majority`
+const db = process.env.MONGO_URI
 
 const connectDB = async () => {
     try {
