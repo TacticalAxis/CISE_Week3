623f11c95cbeb40245a9ca7e1970cbf1240dc691
Nathan <nathan.dsouza316@gmail.com>
2023-08-05 14:21:05 +1200

    Temporarily removed serverless functionality modified vercel json backend nest again

--- backend-week3/src/main.ts
+++ backend-week3/src/main.ts
@@ -1,20 +1,5 @@
-// import { NestFactory } from '@nestjs/core';
-// import { AppModule } from './app.module';
-
-// import * as dotenv from 'dotenv';
-// dotenv.config();
-
-// async function bootstrap() {
-//   const app = await NestFactory.create(AppModule);
-//   app.enableCors();
-//   await app.listen(process.env.SERVER_PORT || 8082);
-// }
-// bootstrap();
-
-
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as serverlessHttp from 'serverless-http';
 
 import * as dotenv from 'dotenv';
 dotenv.config();
@@ -22,14 +7,29 @@ dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  return app.init();
+  await app.listen(process.env.SERVER_PORT || 8082);
 }
+bootstrap();
 
-const server = serverlessHttp(async (req, res) => {
-  if (!req.app) {
-    await bootstrap();
-  }
-  req.app(req, res);
-});
 
-export default server;
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+// import * as serverlessHttp from 'serverless-http';
+
+// import * as dotenv from 'dotenv';
+// dotenv.config();
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   app.enableCors();
+//   return app.init();
+// }
+
+// const server = serverlessHttp(async (req, res) => {
+//   if (!req.app) {
+//     await bootstrap();
+//   }
+//   req.app(req, res);
+// });
+
+// export default server;
